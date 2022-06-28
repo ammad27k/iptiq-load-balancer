973ed4a6d1bb4266ddef7a9ce88b7fe11bdbc1a3
aarshad <ammada@letshego.com>
2022-06-28 20:04:26 +0500

    logger added

--- src/main/kotlin/com/swiss/LoadBalancerApplication.kt
+++ src/main/kotlin/com/swiss/LoadBalancerApplication.kt
@@ -4,8 +4,10 @@ import com.swiss.loadbalancer.checker.ProviderHealthCheckerScheduler
 import com.swiss.loadbalancer.config.LoadBalancerConfigInitializer
 import com.swiss.loadbalancer.factory.LoadBalancerFactory
 import com.swiss.loadbalancer.registry.ProviderRegistrar
+import mu.KotlinLogging
 import java.util.concurrent.CompletableFuture
 
+private val logger = KotlinLogging.logger {}
 fun main() {
 
     LoadBalancerConfigInitializer()
@@ -14,23 +16,11 @@ fun main() {
     val loadBalancer = LoadBalancerFactory.getLoadBalancer(LoadBalancerConfigInitializer.loadBalancerConfig?.config?.loadBalancerType!!)
 
 
-    /*val executors = Executors.newFixedThreadPool(20)
-    CompletableFuture.supplyAsync {  }
-    CompletableFuture.allOf()
-    for(i in 1..20) {
-        executors.submit {
-            try {
-                println(loadBalancer.get())
-            }catch (e : ApplicationException) {
-                println(e.message)
-            }
-        }
-    }*/
     val tasksList: MutableList<CompletableFuture<String>?> = mutableListOf()
     for(i in 1..20) {
         val tasks = CompletableFuture.supplyAsync {
             val result = loadBalancer.get()
-            println(result)
+            logger.debug(result)
             result
         }.exceptionally { e -> println(e)
             ""
--- src/main/kotlin/com/swiss/loadbalancer/config/LoadBalancerConfigInitializer.kt
+++ src/main/kotlin/com/swiss/loadbalancer/config/LoadBalancerConfigInitializer.kt
@@ -2,17 +2,24 @@ package com.swiss.loadbalancer.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import mu.KotlinLogging
 
 class LoadBalancerConfigInitializer {
 
+    private val logger = KotlinLogging.logger {}
     companion object {
         var loadBalancerConfig : LoadBalancerConfig? = null
     }
 
     init {
-        val mapper = ObjectMapper(YAMLFactory())
-        val configFileStream =
-            javaClass.classLoader.getResourceAsStream("load-balancer-config.yml")
-        loadBalancerConfig = mapper.readValue(configFileStream, LoadBalancerConfig::class.java)
+        try {
+            val mapper = ObjectMapper(YAMLFactory())
+            val configFileStream =
+                javaClass.classLoader.getResourceAsStream("load-balancer-config.yml")
+            loadBalancerConfig = mapper.readValue(configFileStream, LoadBalancerConfig::class.java)
+            logger.debug("config initialized successfully")
+        } catch (e : Exception) {
+            logger.error("#LoadBalancerConfigInitializer# Config not initialized", e)
+        }
     }
 }
